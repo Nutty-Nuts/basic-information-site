c78c50842b822d6a25eb7d409b0cdefd48e2a1ee
Nutty-Nuts <vegagerard1130@gmail.com>
2023-10-31 13:45:28 +0800

    improved routing w/ error handling

--- src/index.js
+++ src/index.js
@@ -3,28 +3,37 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    render(req, res, "/", "index.html");
-    render(req, res, "/about", "about.html");
-    render(req, res, "/contact", "contact-me.html");
+    switch (req.url) {
+        case "/":
+            render(req, res, "index.html");
+            break;
+        case "/about":
+            render(req, res, "about.html");
+            break;
+        case "/contact-me":
+            render(req, res, "contact-me.html");
+            break;
+        default:
+            error(res);
+            break;
+    }
 });
 
 const PORT = process.env.PORT || 8080;
 const PUBLIC = "../public/";
 
-function render(req, res, url, file) {
-    if (req.url === url) {
-        fs.readFile(path.join(__dirname, PUBLIC, file), (err, content) => {
-            if (err) {
-                error(req, res);
-                return;
-            }
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content);
-        });
-    }
+function render(req, res, file) {
+    fs.readFile(path.join(__dirname, PUBLIC, file), (err, content) => {
+        if (err) {
+            error(res);
+            return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+    });
 }
 
-function error(req, res) {
+function error(res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<h1>Error</h1>");
 }
